457aa80d870c95aaa205340fc535be8671a3bbd2
soundhar <soundhar@DESKTOP-C5GMPOJ>
2021-09-15 14:55:11 +0530

    Ticket BY Sou

--- LTIAirlines/src/main/java/com/lti/model/Ticket1.java
+++ LTIAirlines/src/main/java/com/lti/model/Ticket1.java
@@ -1,5 +1,19 @@
 package com.lti.model;
 
 public class Ticket1 {
-	
+		int ticketNo;
+		String name;
+		public int getTicketNo() {
+			return ticketNo;
+		}
+		public void setTicketNo(int ticketNo) {
+			this.ticketNo = ticketNo;
+		}
+		public String getName() {
+			return name;
+		}
+		public void setName(String name) {
+			this.name = name;
+		}
+		
 }
